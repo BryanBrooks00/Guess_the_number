82937f237616ae2e8c836ef0226d579dbec359aa
Siarhei Blakhin <76600415+BryanBrooks00@users.noreply.github.com>
2021-11-21 15:19:47 +0200

    Update Level1.java

--- app/src/main/java/blohins/game/ugadaika/Level1.java
+++ app/src/main/java/blohins/game/ugadaika/Level1.java
@@ -9,6 +9,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,11 +18,12 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 import java.util.Random;
-
+final static String TAG = "LOG"
 
 public class Level1 extends AppCompatActivity {
     private AdView adView;
     int counter = 0;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,39 +40,14 @@ public class Level1 extends AppCompatActivity {
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
 
-
-        final EditText input = (EditText) findViewById(R.id.input);
-        final TextView result = (TextView)findViewById(R.id.result);
-        final Button send = (Button)findViewById(R.id.send);
-        Random random = new Random();
-        final int number = random.nextInt(101);
-        final TextView trying = (TextView) findViewById(R.id.trying);
-       send.setOnClickListener(new View.OnClickListener() {
-           @SuppressLint("SetTextI18n")
-           @Override
-           public void onClick(View v) {
-               try {
-                   counter ++ ;
-                   trying.setText(getResources().getString(R.string.attempt)+ " " + counter );
-                   Long num = Long.valueOf(number);
-                   if (Long.valueOf(input.getText().toString()).equals(num)) {
-                       result.setText(getResources().getString(R.string.win));
-                       send.setEnabled(false);
-                   } else if (Long.valueOf(input.getText().toString()) < num) {
-                       result.setText(getResources().getString(R.string.low));
-                       input.setText("");
-                   } else if (Long.valueOf(input.getText().toString()) > num) {
-                       result.setText(getResources().getString(R.string.high));
-                       input.setText("");
-
-                   }
-               } catch (Exception e) {
-
-               }
-           }
-       });
-        Button button_back = (Button) findViewById(R.id.button_back);
-        button_back.setOnClickListener(new View.OnClickListener() {
+        final EditText input_et = (EditText) findViewById(R.id.input_et);
+        final TextView result_tv = (TextView)findViewById(R.id.result_tv);
+        final TextView attempt_tv = (TextView) findViewById(R.id.attempt_tv);
+        final Button btn_send = (Button)findViewById(R.id.btn_send);
+        
+     //btn_back begin
+        Button btn_back = (Button) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
@@ -78,10 +55,46 @@ public class Level1 extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                 } catch (Exception e) {
-
+                    Log.i(TAG, "btn_back exception");
                 }
             }
         });
+        // end
+        
+    }
+    
+    public void getResult(){
+       Random random = new Random();
+        final int number = random.nextInt(101);
+        
+       btn_send.setOnClickListener(new View.OnClickListener() {
+           @SuppressLint("SetTextI18n")
+           @Override
+           public void onClick(View v) {
+               try {
+                   counter ++ ;
+                   String s = "";
+                   attempt_tv.setText(getResources().getString(R.string.attempt)+ " " + counter );
+                   Long num = Long.valueOf(number);
+                   Long user_num = Long.valueOf(input.getText().toString()); 
+                   if (user_num == num) {
+                       s= getResources().getString(R.string.win);
+                   } else if (user_num < num) {
+                       s= getResources().getString(R.string.low);
+                   } else if (user_num > num) {
+                       s = getResources().getString(R.string.high);
+
+                   }
+               } catch (Exception e) {
+                   Log.i(TAG, "getResult exception");
+               }
+           }
+       });
+    }
+    
+    public void setResult(String s){
+        result_tv.setText(s);
+        input.setText("");
     }
 
     //System button Back START
@@ -90,13 +103,14 @@ public class Level1 extends AppCompatActivity {
         super.onBackPressed();
 
         try {
-
             Intent intent = new Intent(Level1.this, GameLevels.class);
             startActivity(intent);
         } catch (Exception e) {
-
+            Log.i(TAG, "onBackPressed exception");
         }
     }
+    //end
+    
 }
 
 
